687890cca378151db8c943f3795d2f62cc2e950f
Emojigit <55009343+Emojigit@users.noreply.github.com>
2022-05-06 20:59:23 +0800

    better display in admin panel

--- main/models.py
+++ main/models.py
@@ -6,3 +6,5 @@ class shortcut(models.Model):
     shortcut_key = models.TextField()
     shortcut_value = models.URLField() # URL of the shortcut target
     shortcut_accesses = models.PositiveIntegerField() # Times the shortcut being accessed
+    def __str__(self):
+        return "{} -> {}, used {} time(s)".format(self.shortcut_key,self.shortcut_value,self.shortcut_accesses)
