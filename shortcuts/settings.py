bc29ba50c68b2910d07e52a90a5ff78fa19cfac0
Emojigit <55009343+Emojigit@users.noreply.github.com>
2022-05-11 20:06:07 +0800

    SECURITY FIX: SECURITY KEY

--- shortcuts/settings.py
+++ shortcuts/settings.py
@@ -20,7 +20,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(0rmo5fs(&f$vt+n)f&sfsv=v3+b%h4bh#@wjnx2+#7@_9-e@r'
+#SECRET_KEY = 'django-insecure-(0rmo5fs(&f$vt+n)f&sfsv=v3+b%h4bh#@wjnx2+#7@_9-e@r'
+def setseckey():
+    global SECRET_KEY
+    with open(BASE_DIR / "secretkey.txt") as k:
+        SECRET_KEY = k.read().strip()
+try:
+    setseckey()
+except FileNotFoundError:
+    from django.core.management.utils import get_random_secret_key
+    with open(BASE_DIR / "secretkey.txt", 'w+') as k:
+        k.write(get_random_secret_key())
+    setseckey()
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
